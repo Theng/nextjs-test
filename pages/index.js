40c9a85de9c5edc1894427f130e4f1790151aa36
Cheng Chungtheng <cheng.chungtheng@sabay.com>
2020-09-23 14:42:07 +0700

    init

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import React, {useState} from 'react'
+
+export default function Home() {
+  const [counter, setCounter] = useState(0)
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <button onClick={()=>setCounter(counter+1)}>Count {counter}</button>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
